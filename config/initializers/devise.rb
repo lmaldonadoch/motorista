4e80cff97ece98e1e465b85d41a01cc26f79f123
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-06-17 10:42:49 -0500

    Added Omniauth and figaro gems for authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['05336d65ad92a1a0bda610b30cf5ce68'], callback_url: "http://localhost:3000/users/auth/facebook/callback"
 end
