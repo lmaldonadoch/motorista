58724f4eb7596998f89d94396c261d263098fd70
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-06-18 20:13:10 -0500

    Corrected stylelint issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  devise :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, omniauth_providers: [:facebook]
   has_many :articles, class_name: 'Article', foreign_key: 'authorid'
   has_many :votes, class_name: 'Vote', foreign_key: 'userid'
 
@@ -14,8 +14,8 @@ public
 
 def new_with_session(params, session)
   super.tap do |user|
-    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-      user.email = data["email"] if user.email.blank?
+    if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+      user.email = data['email'] if user.email.blank?
     end
   end
 end
@@ -23,8 +23,8 @@ end
 def from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.name = auth.info.name   # assuming the user model has a name
+    user.password = Devise.friendly_token[0, 20]
+    user.name = auth.info.name # assuming the user model has a name
     user.image = auth.info.image # assuming the user model has an image
   end
 end
